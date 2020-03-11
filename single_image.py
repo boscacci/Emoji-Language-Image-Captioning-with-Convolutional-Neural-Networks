ea2819031951dea6d78eb556e417f54b9b926f6a
boscacci <cinemarob1@gmail.com>
2020-03-10 20:15:56 -0400

    Generate new predictions

---
+++ single_image.py
@@ -0,0 +1,45 @@
+import torch
+import numpy as np
+from torchvision import transforms
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+from utils import ConvNet
+from utils import int_to_emoji_dict
+from utils import NUM_CLASSES
+
+from PIL import Image
+
+MODEL_NAME = "model.ckpt"
+
+device = "cpu"
+model = ConvNet(NUM_CLASSES).to(device)
+model.load_state_dict(
+    torch.load(MODEL_NAME, map_location=lambda storage, loc: storage)
+)
+
+imsize = 256
+
+
+def make_pred(image_path):
+    img = image_loader(image_path)
+    model.eval()
+    inputs = Variable(img).to(device)
+    logits = model.forward(inputs)
+    ps = F.softmax(logits, dim=1)
+    topk = ps.cpu().topk(5)
+    top_5_ints = topk.indices.tolist()[0]
+    return [int_to_emoji_dict[x] for x in top_5_ints]
+
+
+def image_loader(image_path):
+    """ Scales, crops, and normalizes an image file for a PyTorch model,
+        returns an Numpy array
+    """
+    image = Image.open(image_path).convert("RGB")
+    preprocess = transforms.Compose(
+        [transforms.Resize((256, 256)), transforms.ToTensor()]
+    )
+    image = np.expand_dims(preprocess(image), 0)
+    tensor = torch.from_numpy(image)
+    return tensor
--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 from utils import ConvNet
 from utils import CocoCaptions
 import utils
-import torch 
+import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 import numpy as np
@@ -10,60 +10,63 @@ import pickle
 
 ### Hyperparameters
 
-BATCH_SIZE = utils.BATCH_SIZE
+BATCH_SIZE = 50
 NUM_CLASSES = utils.NUM_CLASSES
 
 
-test_dir = 'resized_val2017/'
+test_dir = "resized_val2017/"
 
-ann_file_test = 'annotations/captions_val2017.json'
+ann_file_test = "annotations/captions_val2017.json"
 
-test_dataset = CocoCaptions(root=test_dir, annFile=ann_file_test, transform=transforms.ToTensor())
+test_dataset = CocoCaptions(
+    root=test_dir, annFile=ann_file_test, transform=transforms.ToTensor()
+)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                           batch_size=BATCH_SIZE, 
-                                           shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=True
+)
 
-MODEL_NAME = 'model.ckpt'
+MODEL_NAME = "model.ckpt"
 
 device = None
 model = None
 
-if torch.cuda.is_available(): 
-    device = torch.device('cuda:0')
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
     model = ConvNet(NUM_CLASSES).to(device)
     model.load_state_dict(torch.load(MODEL_NAME))
-else: 
-    device = 'cpu'
+else:
+    device = "cpu"
     model = ConvNet(NUM_CLASSES).to(device)
-    model.load_state_dict(torch.load(MODEL_NAME, map_location=lambda storage, loc: storage))
+    model.load_state_dict(
+        torch.load(MODEL_NAME, map_location=lambda storage, loc: storage)
+    )
 
 
 ### Test the model
-
-predicted = []
-actual = []
 
 id_to_prediction_dict = {}
 id_to_actual_dict = {}
 
 for i, (image_ids, images, labels) in enumerate(test_loader):
     images = images.to(device)
-
     # Forward pass
     outputs = model(images)
     softmax = torch.nn.Softmax()
 
     for j, scores in enumerate(softmax(outputs).tolist()):
-        id_to_prediction_dict[int(image_ids[j])] = [utils.int_to_emoji_dict[x] for x in np.argsort(scores)[::-1][:5]]
-    for j, label in enumerate(labels.tolist()):
-        id_to_actual_dict[int(image_ids[j])] = [utils.int_to_emoji_dict[x] for x in label]
+        id_to_prediction_dict[int(image_ids[j])] = [
+            utils.int_to_emoji_dict[x] for x in np.argsort(scores)[::-1][:5]
+        ]
 
-with open('id_to_prediction_dict.pkl', 'wb') as f:
+    for j, label in enumerate(labels.tolist()):
+        id_to_actual_dict[int(image_ids[j])] = [
+            utils.int_to_emoji_dict[x] for x in label
+        ]
+
+with open("id_to_prediction_dict.pkl", "wb") as f:
     pickle.dump(id_to_prediction_dict, f)
 
-with open('id_to_actual_dict.pkl', 'wb') as f:
+with open("id_to_actual_dict.pkl", "wb") as f:
     pickle.dump(id_to_actual_dict, f)
-
-
 
